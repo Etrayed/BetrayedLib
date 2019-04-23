ee1532fa5fdf776da7aeaf2c25b144365aa5f7f7
Etrayed <etrayedyt@gmail.com>
2019-04-23 07:34:50 +0200

    Added missing expression.

--- BukkitCommandAPI/src/main/java/de/etrayed/betrayedlib/commandapi/execute/RemoteCommandExecutor.java
+++ BukkitCommandAPI/src/main/java/de/etrayed/betrayedlib/commandapi/execute/RemoteCommandExecutor.java
@@ -6,6 +6,7 @@ import de.etrayed.betrayedlib.commandapi.annotation.CommandMethod;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.reflect.Method;
@@ -30,6 +31,9 @@ public class RemoteCommandExecutor implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(commandMethod.blockConsoleCommandSenders() && !(commandSender instanceof Player))
+            return false;
+
         if(!commandMethod.permission().isEmpty() && !commandSender.hasPermission(commandMethod.permission())) {
             if(commandMethod.useDefaultNoPermissionMessage()) {
                 commandSender.sendMessage(CommandAPI.getNoPermissionMessage());
